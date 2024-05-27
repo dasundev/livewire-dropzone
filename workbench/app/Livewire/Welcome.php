a50518042bb3aeddc53970fa98caef5f2edab4c7
Dasun Tharanga <hello@dasun.dev>
2024-05-27 17:09:27 +0530

    test: create livewire component

---
+++ workbench/app/Livewire/Welcome.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Workbench\App\Livewire;
+
+use Illuminate\Contracts\View\View;
+use Livewire\Attributes\Layout;
+use Livewire\Component;
+
+class Welcome extends Component
+{
+    public $welcome;
+
+    #[Layout('workbench::layouts.app')]
+    public function render(): View
+    {
+        return view('workbench::welcome');
+    }
+}
