6cceca1d22e10cf5a04cb2ee9799ceeb9fbaf509
Dasun Tharanga <hello@dasun.dev>
2024-11-23 10:14:24 +0530

    test(workbench): update views

--- workbench/resources/views/layouts/app.blade.php
+++ workbench/resources/views/layouts/app.blade.php
@@ -4,14 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    
     <link rel="stylesheet" href="{{ asset('vendor/livewire-dropzone/styles.css') }}">
-
+    
     <title>Livewire Dropzone</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-
-{{ $slot }}
-
+<body class="max-w-2xl mx-auto min-h-screen flex justify-center items-center">
+    {{ $slot }}
 </body>
 </html>
--- workbench/resources/views/welcome.blade.php
+++ workbench/resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<form wire:submit="submit">
+<form wire:submit="submit" class="w-full">
     <livewire:dropzone
             wire:model="photos"
             :rules="['image','mimes:png,jpeg','max:10420']"
