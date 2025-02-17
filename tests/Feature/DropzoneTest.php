7ffec9f883051422ea4d88d87d17f47f860a020c
eafarooqi <e_a_farooqi@hotmail.com>
2025-02-17 11:23:51 +0100

    Fix: remember files after validation error on the form on any other field

--- tests/Feature/DropzoneTest.php
+++ tests/Feature/DropzoneTest.php
@@ -18,6 +18,34 @@ it('accepts and sets multiple parameter correctly', function () {
         ->assertSet('multiple', true);
 });
 
+it('accepts and sets files parameter correctly', function () {
+    $file1 = UploadedFile::fake()->image('file1.png');
+    $file2 = UploadedFile::fake()->image('file2.jpg');
+
+    $files = [
+        [
+            'name' => $file1->getClientOriginalName(),
+            'path' => $file1->path(),
+            'extension' => $file1->extension(),
+            'temporaryUrl' => $file1->path(),
+            'size' => $file1->getSize(),
+            'tmpFilename' => $file1->getFilename(),
+        ],
+        [
+            'name' => $file2->getClientOriginalName(),
+            'path' => $file2->path(),
+            'extension' => $file2->extension(),
+            'temporaryUrl' => $file2->path(),
+            'size' => $file2->getSize(),
+            'tmpFilename' => $file2->getFilename(),
+        ],
+    ];
+
+    Livewire\Livewire::test(Dropzone::class, ['files' => $files])
+        ->assertSet('files', $files);
+});
+
+
 it('can upload file', function () {
     $dropzone = Livewire\Livewire::test(Dropzone::class);
 
